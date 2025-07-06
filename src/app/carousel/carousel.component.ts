bd7d216d9e1abb83b49689b7f91298bf462827f8
doncorleone <linus@wieland.click>
2025-07-06 17:28:06 +0200

    1:1 angular

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -4,23 +4,7 @@ import { NgTemplateOutlet } from '@angular/common';
 @Component({
   selector: 'app-carousel',
   imports: [NgTemplateOutlet],
-  template: `
-    <div class="carousel">
-      <div class="carousel-inner">
-        @for (slide of slides(); track slide; let i = $index) {
-          <div class="carousel-item" [class.active]="i === activeIndex">
-            <ng-container *ngTemplateOutlet="slide"></ng-container>
-          </div>
-        }
-      </div>
-      <button class="carousel-control-prev" (click)="prev()" aria-label="Previous">
-        <span class="carousel-control-prev-icon"></span>
-      </button>
-      <button class="carousel-control-next" (click)="next()" aria-label="Next">
-        <span class="carousel-control-next-icon"></span>
-      </button>
-    </div>
-    `,
+  templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
